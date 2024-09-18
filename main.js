4cb8d32b9deab432b41d1d507d38052500355961
Anna Teng <s12345abcd5@gmail.com>
2024-09-18 13:38:46 +0800

    新增courseVideo & 修改courseInfo

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
-console.log("Hello world!");
+// courseVideo.html header影片右邊選單，點擊加active換color顏色
+$('.course-video li').click(function (event) {
+    $(this).toggleClass('active').siblings().removeClass('active');
+});
