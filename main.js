2e46434d56b645a03e56668cfcee71de167db924
Anna Teng <s12345abcd5@gmail.com>
2024-10-01 14:47:41 +0800

    改header+每頁載入jQuery

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
+// header 選單點擊套用樣式，同時移除其他選項樣式
+$('.header-list-item a').click(function (event) {
+    $(this).addClass('click-active').parents().siblings().find('a').removeClass('click-active');
+});
+
+// header 課程探索點擊套用樣式，同時移除其他選項樣式
+$('.collapse-list-item li a').click(function (event) {
+    $(this).addClass('list-active').parents().siblings().find('a').removeClass('list-active');
+});
+
+
 // courseVideo.html header影片右邊選單，點擊加active換color顏色
 $('.course-video li').click(function (event) {
     $(this).toggleClass('active').siblings().removeClass('active');
